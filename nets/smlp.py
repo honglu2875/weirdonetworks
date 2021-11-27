31881c6b3c2f6dd07c39a3be6186cb7589d9ba8d
Void <honglu2875@gmail.com>
2021-11-27 08:35:00 -0500

    optimize

--- nets/smlp.py
+++ nets/smlp.py
@@ -2,11 +2,11 @@ from .utils import *
 import sonnet as snt
 import tensorflow as tf
 
-class SMLP(snt.Module):
+class SMLP_test(snt.Module):
 
     def __init__(self, l1=5, l2=10):
 
-        super(SMLP, self).__init__()
+        super(SMLP_test, self).__init__()
         self.flatten = snt.Flatten()
         self.net1 = snt.Linear(l1, name="net1")
         self.net2 = snt.Linear(l2, name="net2")
@@ -15,22 +15,15 @@ class SMLP(snt.Module):
 
     def __call__(self, images, is_training=False):
 
-        output = tf.expand_dims(self.flatten(images), axis=-1)
-        out = None
-        for m in range(self._mat.shape[0]):
-            mat = tf.repeat(tf.expand_dims(self._mat[m], axis=0), repeats=output.shape[0], axis=0)
-            res = self.flatten(tf.matmul(mat, output))
+        output = tf.expand_dims(self.flatten(images), axis=-1) # Make it into a vector of shape (*, 784, 1)
+        output = tf.repeat(tf.expand_dims(output, axis=1), repeats=self._mat.shape[0], axis=1) # Repeat to match the number of transformations, resulting in shape (*, number of trans, 784, 1)
+        output = tf.matmul(self._mat, output) # Apply the matrices. The matrices will be applied on the last three coordinates by convention
 
-            res = tf.nn.relu(self.net1(res))
-            res = tf.nn.relu(self.net2(res))
-
-            if out is None:
-                out = tf.expand_dims(res, axis=0)
-            else:
-                out = tf.concat([out, tf.expand_dims(res, axis=0)], axis=0)
+        output = self.flatten(output)
+        output = tf.nn.relu(self.net1(output))
+        output = tf.nn.relu(self.net2(output))
 
 
-        output = tf.transpose(out, perm=[1,0,2])
         output = self.logits(self.flatten(output))
 
         return {"logits": output, "softmax": tf.nn.softmax(output)}
